4d7e305c3642998b90c965afea0931a6a0174b95
dengyongbin <dengyongbin@loopeer.com>
2015-12-22 19:15:01 +0800

    add push migrate and submit

--- src/Seeds/InitBackendSeeder.php
+++ src/Seeds/InitBackendSeeder.php
@@ -14,6 +14,12 @@ class InitBackendSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('role_user')->truncate();
+        DB::table('permissions')->truncate();
+        DB::table('permission_role')->truncate();
+
         DB::table('users')->insert([
             'name' => 'bd',
             'email' => 'bd@loopeer.com',
